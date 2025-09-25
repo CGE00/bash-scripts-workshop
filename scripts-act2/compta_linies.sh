b1a791df4f764de42a1d72f0d269781dbfde8d87
root <godoychristian2k@gmail.com>
2025-09-25 16:26:00 +0200

    Actualitzat

--- scripts-act2/compta_linies.sh
+++ scripts-act2/compta_linies.sh
@@ -2,11 +2,11 @@
 
 #Christian Godoy Esteoa
 #25/09/2025
-#Aquest script compta les linies de l'arxiu psswd.
+#Aquest script compta les linies de l'arxiu psswd i si hi ha errors ho pasa a l arxiu errors.log.
 
 echo "Començant el recompte de línies útils..."
 
-cat /etc/passwd | wc -l | grep -v '^$' > linies.log 2> errors.log
+cat /etc/passwd | grep -v '^$'| wc -l > linies.log 2> errors.log
 date >> linies.log
 
 echo "Procés completat! Consulta linies.log i errors.log"
